30b8431cc6a8d1771c05c83e2810e44435194b4c
Priya Pandey <137922367+priyapandey0@users.noreply.github.com>
2024-02-12 08:24:19 +0000

    First Commit

---
+++ Basics/datatype.js
@@ -0,0 +1,15 @@
+"use strict";   // treat all js code as newer version
+
+console.log(3+6)
+
+
+console.log("Priya")
+
+
+let name = "Priya"
+let age = 18
+let isLoggedIn = false  
+let state;
+
+console.log(typeof undefined);
+
