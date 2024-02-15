536a6a07b03c9e9fd25f7f062c3bcb7319a77bcd
Priya Pandey <137922367+priyapandey0@users.noreply.github.com>
2024-02-15 07:21:06 +0000

    Third Commit

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "33dhg"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" = 33
+// "33abc" = NaN
+// true = 1; false= 0
+
+let isLoggedIn = "Priyaaaa"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
