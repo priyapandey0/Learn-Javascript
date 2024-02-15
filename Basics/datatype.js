f3b56f1f2c43a8c7a8b8bd5f1c5580cd4d923443
Priya Pandey <137922367+priyapandey0@users.noreply.github.com>
2024-02-15 07:10:34 +0000

    Second Commit

--- Basics/datatype.js
+++ Basics/datatype.js
@@ -2,14 +2,10 @@
 
 console.log(3+6)
 
-
 console.log("Priya")
-
 
 let name = "Priya"
 let age = 18
 let isLoggedIn = false  
 let state;
-
 console.log(typeof undefined);
-
---
+++ Basics/variables.js
@@ -0,0 +1,20 @@
+const accountId = 234223
+let accountEmail = "WP@gmail.com"
+var accountPassword = "5367"
+accountCity = "AYodhya"
+let accountState;
+
+// accountId = 2  //not allowed
+
+// console.log(accountId);
+
+accountEmail = "hc@hc.com"
+accountPassword = "5436546"
+accountCity = "Ayodhya"     
+
+console.log(accountId);
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
